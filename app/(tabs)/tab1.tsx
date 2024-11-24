aa3e86a0341818c4b9d0ae671888d57466ff31e2
phkai <phkai16@gmail.com>
2024-11-24 23:07:19 +0700

    init project

---
+++ app/(tabs)/tab1.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Tab1() {
+  return (
+    <View className='flex-1 items-center justify-center bg-gray-50'>
+      <Text className='text-blue-800'>Tab 1 Content</Text>
+    </View>
+  )
+}
